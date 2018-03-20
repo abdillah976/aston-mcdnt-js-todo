bfc48d4fd79b5616490f730822096d10be316a72
Abdillah <abdillah.mohamed@hotmail.fr>
2018-03-20 18:02:35 +0100

    Feature localStorage

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
     // var tache = document.getElementById("inptTache").value;
     var liste = document.getElementById("liste");
+    var index = 0;
 
     // Matérialise un element d'une liste
     function liTache() {
         var tache = document.getElementById("inptTache").value;
         if(tache != ""){
             var li = document.createElement('li');
-            li.innerText = document.getElementById("inptTache").value;
+            // Ajoute un id
+            li.setAttribute("id","tache"+tache);
+            li.innerText = tache;
             li.innerHTML += ' <button onclick="supprimeTache(this.parentNode);">Supprimer</button>';
             liste.appendChild(li);
-            localStorage.setItem("tache."+tache, tache);
+            localStorage.setItem(index, tache);
+            index +=1;
+            limitStorage(index);
         }
     }
 
@@ -21,12 +26,31 @@
     // Supprime l'element passer en paramétre de la liste
     function supprimeTache(li) {
         li.parentNode.removeChild(li);
-        
+        // Supprime la data liée à la tache
+        supprimeStorage(li);
     }
 
     // Recharge les données de la liste
     function loadData(){
-        for(var i=0; i < localStorage.length; i++){
-
+        if(localStorage.length != 0){
+            for(var i=0; i < localStorage.length; i++){
+                var li = document.createElement('li');
+                // Ajoute un id
+                li.setAttribute("id",localStorage[i]);
+                console.log(localStorage);
+                li.innerText = localStorage.getItem(localStorage[i]);
+                li.innerHTML += ' <button onclick="supprimeTache(this.parentNode);">Supprimer</button>';
+                liste.appendChild(li);
+            }
         }
+    }
+    // Supprime la storage passée en paramétre
+    function supprimeStorage(li){
+        if(li.getAttribute("id") == localStorage.getItem(li.getAttribute("id"))){
+            localStorage.removeItem(li.getAttribute("id"));
+        }
+    }
+    // Enrigistre le dernier index du localstorage
+    function limitStorage(id) {
+        localStorage.setItem("index",id);
     }
